dbb2c0a8cba4b6afde0ae2d9ffd796220b27bfea
Rojaao <roggerpk@gmail.com>
2025-06-21 10:00:10 -0300

    Update estrategia.py

--- estrategia.py
+++ estrategia.py
@@ -1,5 +1,6 @@
+
 def analisar_ticks_famped(ticks):
-    menores_ou_iguais_a_4 = [d for d in ticks if d <= 4]
-    if len(menores_ou_iguais_a_4) >= 6:
+    menores_que_4 = [d for d in ticks if d < 5]
+    if len(menores_que_4) >= 6:
         return {"entrada": "DIGITOVER 4", "estrategia": "Famped"}
     return {"entrada": "ESPERAR", "estrategia": "Famped"}
