2ccce3523c6ee8d748077767ccd3eb2244f1bf0e
Rojaao <roggerpk@gmail.com>
2025-06-20 22:35:28 -0300

    Delete utils.py

--- utils.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-
-def plot_resultados(resultados):
-    fig, ax = plt.subplots(figsize=(6, 2))
-    cumulativo = [sum(resultados[:i+1]) for i in range(len(resultados))]
-    ax.plot(cumulativo, marker='o')
-    ax.set_title("Lucro/Prejuízo Acumulado")
-    ax.grid(True)
-    return fig
