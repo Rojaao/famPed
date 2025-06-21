41c95ad0b10b6b22158e2fda4fb047ce235cc900
Rojaao <roggerpk@gmail.com>
2025-06-21 09:47:49 -0300

    Delete estrategia.py

--- estrategia.py
+++
@@ -1,6 +0,0 @@
-
-def analisar_ticks_famped(ticks):
-    menores_que_4 = [d for d in ticks if d < 4]
-    if len(menores_que_4) >= 6:
-        return {"entrada": "DIGITOVER 4", "estrategia": "Famped"}
-    return {"entrada": "ESPERAR", "estrategia": "Famped"}
